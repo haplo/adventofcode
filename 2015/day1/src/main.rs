0d039b756cc9bca7896bd04e804ce585e78a7c3d
Fidel Ramos <f@fidelramos.net>
2021-10-14 22:49:34 +0100

    2015 Day 1, Part 2

--- 2015/day1/src/main.rs
+++ 2015/day1/src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
     let mut total = 0;
+    let mut basement = 0;
     let input = std::fs::read_to_string("input.txt").unwrap();
-    for char in input.chars() {
+    for (i, char) in input.chars().enumerate() {
         match char {
             '(' => total += 1,
             ')' => total -= 1,
             _ => (),
         }
+        if total <= -1 && basement == 0 {
+            basement = i + 1;
+        }
     }
-    println!("Floor {}", total)
+    println!("Floor {}", total);
+    println!("Basement first entered at position {}", basement);
 }
