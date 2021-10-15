c88e5e6eb71ee174823c28d0fab5cb607d49d046
Fidel Ramos <f@fidelramos.net>
2021-10-15 08:18:24 +0100

    2015 Day 2, Part 2

--- 2015/day2/src/main.rs
+++ 2015/day2/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::sorted;
 use itertools::Itertools;
 
 fn paper_for_present(l: u32, w: u32, h: u32) -> u32 {
@@ -15,8 +16,19 @@ fn paper_for_present(l: u32, w: u32, h: u32) -> u32 {
     paper
 }
 
+fn ribbon_for_present(l: u32, w: u32, h: u32) -> u32 {
+    let (side1, side2) = sorted(vec![l, w, h].into_iter())
+        .take(2)
+        .collect_tuple()
+        .unwrap();
+    let wrap = side1 + side1 + side2 + side2;
+    let bow = l * w * h;
+    wrap + bow
+}
+
 fn main() {
-    let mut total = 0;
+    let mut paper = 0;
+    let mut ribbon = 0;
     let input = std::fs::read_to_string("input.txt").unwrap();
     for dim in input.lines() {
         let (l, w, h) = dim
@@ -25,9 +37,11 @@ fn main() {
             .map(Result::unwrap)
             .collect_tuple()
             .unwrap();
-        total += paper_for_present(l, w, h)
+        paper += paper_for_present(l, w, h);
+        ribbon += ribbon_for_present(l, w, h);
     }
-    println!("Need {} sq.ft. of wrapping paper", total)
+    println!("Need {} sq.ft. of wrapping paper", paper);
+    println!("Need {} ft. of ribbon", ribbon);
 }
 
 #[cfg(test)]
@@ -38,5 +52,18 @@ mod tests {
     fn test_paper_for_present() {
         assert_eq!(paper_for_present(2, 3, 4), 58);
         assert_eq!(paper_for_present(1, 1, 10), 43);
+    }
+
+    #[test]
+    fn test_ribbon_for_present() {
+        assert_eq!(ribbon_for_present(2, 3, 4), 34);
+        assert_eq!(ribbon_for_present(2, 4, 3), 34);
+        assert_eq!(ribbon_for_present(3, 2, 4), 34);
+        assert_eq!(ribbon_for_present(3, 4, 2), 34);
+        assert_eq!(ribbon_for_present(4, 2, 3), 34);
+        assert_eq!(ribbon_for_present(4, 3, 2), 34);
+        assert_eq!(ribbon_for_present(1, 1, 10), 14);
+        assert_eq!(ribbon_for_present(1, 10, 1), 14);
+        assert_eq!(ribbon_for_present(10, 1, 1), 14);
     }
 }
