3248c8a94281ea8b17c09c023cb018f1367ce61c
Fidel Ramos <f@fidelramos.net>
2021-10-20 23:07:29 +0100

    2015 Day 8, Part 2

--- 2015/day8/src/main.rs
+++ 2015/day8/src/main.rs
@@ -30,17 +30,44 @@ fn count_memory_chars(s: &str) -> usize {
     count
 }
 
+fn count_escaped_chars(s: &str) -> usize {
+    let mut escaped = String::with_capacity(s.len() * 2);
+    escaped.push('"');
+    let mut char_iter = s.chars();
+    while let Some(c) = char_iter.next() {
+        match c {
+            '\\' => {
+                escaped.push('\\');
+                escaped.push('\\');
+            }
+            '\"' => {
+                escaped.push('\\');
+                escaped.push('\"');
+            }
+            c => escaped.push(c),
+        }
+    }
+    escaped.push('"');
+    escaped.len()
+}
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let mut total_code_chars = 0;
     let mut total_memory_chars = 0;
+    let mut total_escaped_chars = 0;
     for line in input.lines() {
         total_code_chars += count_code_chars(line);
         total_memory_chars += count_memory_chars(line);
+        total_escaped_chars += count_escaped_chars(line);
     }
     println!(
-        "Code chars - Memory chars = {}",
+        "Part 1: Code chars - Memory chars = {}",
         total_code_chars - total_memory_chars
+    );
+    println!(
+        "Part 2: Escaped chars - Code chars = {}",
+        total_escaped_chars - total_code_chars
     );
 }
 
@@ -54,6 +81,14 @@ mod tests {
         assert_eq!(count_code_chars(&"\"abc\""), 5);
         assert_eq!(count_code_chars(&"\"aaa\\\"aaa\""), 10);
         assert_eq!(count_code_chars(&"\"\\x27\""), 6);
+    }
+
+    #[test]
+    fn test_count_escaped_chars() {
+        assert_eq!(count_escaped_chars(&"\"\""), 6);
+        assert_eq!(count_escaped_chars(&"\"abc\""), 9);
+        assert_eq!(count_escaped_chars(&"\"aaa\\\"aaa\""), 16);
+        assert_eq!(count_escaped_chars(&"\"\\x27\""), 11);
     }
 
     #[test]
