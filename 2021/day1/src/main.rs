3cebb8221e0c0f928006dfa46cf4f28d0af1d2b0
Fidel Ramos <f@fidelramos.net>
2021-12-01 08:50:01 +0000

    2021 Day 1, Part 2

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,21 +1,32 @@
-fn count_increases(measures: impl Iterator<Item = i32>) -> i32 {
-    let mut peekiter = measures.peekable();
+fn count_increases<'a>(measures: &Vec<i32>, size: usize) -> i32 {
     let mut count = 0;
-    while let Some(m) = peekiter.next() {
-        if let Some(&second) = peekiter.peek() {
-            if second > m {
-                count += 1
+    let mut windowiter = measures.windows(size);
+    let mut prev: Option<i32> = None;
+    while let Some(w) = windowiter.next() {
+        let current: i32 = w.iter().sum();
+        if let Some(p) = prev {
+            if current > p {
+                count += 1;
             }
         }
+        prev = Some(current);
     }
     count
 }
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let measures = input.lines().map(|l| l.parse::<i32>().unwrap());
-    let count = count_increases(measures);
-    println!("{} measures larger than the previous measure", count);
+    let measures: Vec<i32> = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+    let count_window_1 = count_increases(&measures, 1);
+    let count_window_3 = count_increases(&measures, 3);
+    println!(
+        "{} measures larger than the previous measure",
+        count_window_1
+    );
+    println!(
+        "{} measures larger than the previous measure using a sliding window of 3",
+        count_window_3
+    );
 }
 
 #[cfg(test)]
@@ -24,9 +35,16 @@ mod tests {
 
     #[test]
     fn test_count_increases() {
-        assert_eq!(count_increases([].into_iter()), 0);
-        assert_eq!(count_increases(vec![1, 2, 3, 2, 2, 3, 1, 3].into_iter()), 4);
-        assert_eq!(count_increases(vec![1, 1, 1, 1].into_iter()), 0);
-        assert_eq!(count_increases(vec![4, 3, 2, 1].into_iter()), 0);
+        // window of size 1, for Part 1
+        assert_eq!(count_increases(&Vec::<i32>::new(), 1), 0);
+        assert_eq!(count_increases(&vec![1, 2, 3, 2, 2, 3, 1, 3], 1), 4);
+        assert_eq!(count_increases(&vec![1, 1, 1, 1], 1), 0);
+        assert_eq!(count_increases(&vec![4, 3, 2, 1], 1), 0);
+
+        // window of size 3, for Part 2
+        assert_eq!(count_increases(&Vec::<i32>::new(), 3), 0);
+        assert_eq!(count_increases(&vec![1, 2, 3, 2, 2, 3, 1, 3], 3), 2);
+        assert_eq!(count_increases(&vec![1, 1, 1, 1], 3), 0);
+        assert_eq!(count_increases(&vec![4, 3, 2, 1], 3), 0);
     }
 }
