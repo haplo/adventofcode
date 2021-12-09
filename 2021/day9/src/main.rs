bd69d69ef46f9ac109f7658a461e028788140dc9
Fidel Ramos <f@fidelramos.net>
2021-12-09 23:23:43 +0000

    2021 Day 9, Part 2

--- 2021/day9/src/main.rs
+++ 2021/day9/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashSet;
+
 type Height = u8;
+const BASIN_LIMIT: Height = 9;
 
 #[derive(Debug, PartialEq)]
 struct Grid {
@@ -28,29 +31,54 @@ impl Grid {
         }
     }
 
-    // true if point at index i is a low point
-    fn is_low_point(&self, i: usize) -> bool {
+    fn adjacents(&self, i: usize) -> Vec<usize> {
         let c = self.columns;
         let p = &self.points;
-        let v = p[i];
-        let mut is_low = true;
+        let mut adj: Vec<usize> = vec![];
         if i >= c {
-            // not first column, look above
-            is_low &= v < p[i - c];
+            // not at the first column, use above neighbor
+            adj.push(i - c);
         }
-        if is_low && i <= p.len() - c {
-            // not last column, look below
-            is_low &= v < p[i + c];
+        if i > 0 && i % c != 0 {
+            // not at the left border, use left neighbor
+            adj.push(i - 1);
         }
-        if is_low && i > 0 && i % c != 0 {
-            // not left border, look left
-            is_low &= v < p[i - 1];
+        if (i + 1) % c != 0 {
+            // not at the right border, use right neighbor
+            adj.push(i + 1);
         }
-        if is_low && (i + 1) % c != 0 {
-            // not right border, look right
-            is_low &= v < p[i + 1];
+        if i < p.len() - c {
+            // not at the last column, use below neighbor
+            adj.push(i + c);
         }
-        is_low
+        adj
+    }
+
+    // return basin size around point at index i
+    fn basin(&self, i: usize) -> u32 {
+        let mut visited = HashSet::<usize>::new();
+        let mut to_visit = Vec::<usize>::new();
+        to_visit.push(i);
+        while !to_visit.is_empty() {
+            let i = to_visit.pop().unwrap();
+            visited.insert(i);
+            let unvisited_adj: Vec<usize> = self
+                .adjacents(i)
+                .into_iter()
+                .filter(|x| self.points[*x] != BASIN_LIMIT && !visited.contains(x))
+                .collect();
+            to_visit.extend(unvisited_adj);
+        }
+        visited.len() as u32
+    }
+
+    // true if point at index i is a low point
+    fn is_low_point(&self, i: usize) -> bool {
+        let v = self.points[i];
+        self.adjacents(i)
+            .iter()
+            .map(|a| self.points[*a])
+            .all(|n| v < n)
     }
 }
 
@@ -61,14 +89,32 @@ fn risk_level(height: Height) -> u32 {
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let grid = Grid::from_str(&input);
+    // grid position of the low points
+    let low_points: Vec<usize> = grid
+        .points
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| grid.is_low_point(*i))
+        .map(|(i, _)| i)
+        .collect();
     println!(
         "Total risk level = {}",
-        grid.points
+        low_points
             .iter()
-            .enumerate()
-            .filter(|(i, _)| grid.is_low_point(*i))
-            .map(|(_, h)| risk_level(*h))
+            .map(|x| risk_level(grid.points[*x]))
             .sum::<u32>(),
+    );
+    // sizes of all basins, calculations rely on the fact that each point belongs to only
+    // one basin
+    let mut basins = low_points
+        .iter()
+        .map(|p| grid.basin(*p))
+        .collect::<Vec<u32>>();
+    // sort will put smallest basins first, that's why rev() is used later
+    basins.sort_unstable();
+    println!(
+        "Multiplication of 3 largest basins: = {}",
+        basins.iter().rev().take(3).fold(1, |acc, b| acc * b)
     );
 }
 
@@ -94,6 +140,28 @@ mod tests {
                 ]
             }
         )
+    }
+
+    #[test]
+    fn test_adj() {
+        let grid = Grid::from_str(HEIGHTMAP);
+        assert_eq!(grid.adjacents(0), vec![1, 10]);
+        assert_eq!(grid.adjacents(5), vec![4, 6, 15]);
+        assert_eq!(grid.adjacents(9), vec![8, 19]);
+        assert_eq!(grid.adjacents(10), vec![0, 11, 20]);
+        assert_eq!(grid.adjacents(15), vec![5, 14, 16, 25]);
+        assert_eq!(grid.adjacents(20), vec![10, 21, 30]);
+        assert_eq!(grid.adjacents(25), vec![15, 24, 26, 35]);
+        assert_eq!(grid.adjacents(49), vec![39, 48]);
+    }
+
+    #[test]
+    fn test_basin() {
+        let grid = Grid::from_str(HEIGHTMAP);
+        assert_eq!(grid.basin(1), 3);
+        assert_eq!(grid.basin(9), 9);
+        assert_eq!(grid.basin(22), 14);
+        assert_eq!(grid.basin(46), 9);
     }
 
     #[test]
