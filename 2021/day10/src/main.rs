15238c76a5d5622e26457b68c0fe9352285ba398
Fidel Ramos <f@fidelramos.net>
2021-12-10 09:47:51 +0000

    2021 Day 10, Part 2

--- 2021/day10/src/main.rs
+++ 2021/day10/src/main.rs
@@ -1,9 +1,20 @@
-const SCORE_PAREN: u32 = 3;
-const SCORE_SQUARE: u32 = 57;
-const SCORE_CURLY: u32 = 1197;
-const SCORE_ANGLE: u32 = 25137;
+const CORRUPT_PAREN: u32 = 3;
+const CORRUPT_SQUARE: u32 = 57;
+const CORRUPT_CURLY: u32 = 1197;
+const CORRUPT_ANGLE: u32 = 25137;
+const INCOMPLETE_PAREN: u64 = 1;
+const INCOMPLETE_SQUARE: u64 = 2;
+const INCOMPLETE_CURLY: u64 = 3;
+const INCOMPLETE_ANGLE: u64 = 4;
 
-fn score_line(line: &str) -> u32 {
+#[derive(Debug, PartialEq)]
+enum LineResult {
+    Corrupt(u32),
+    Incomplete(u64),
+    Valid,
+}
+
+fn score_line(line: &str) -> LineResult {
     let mut stack = Vec::<char>::new();
     for c in line.chars() {
         match c {
@@ -13,35 +24,71 @@ fn score_line(line: &str) -> u32 {
             '<' => stack.push('>'),
             ')' => {
                 if stack.pop() != Some(')') {
-                    return SCORE_PAREN;
+                    return LineResult::Corrupt(CORRUPT_PAREN);
                 }
             }
             ']' => {
                 if stack.pop() != Some(']') {
-                    return SCORE_SQUARE;
+                    return LineResult::Corrupt(CORRUPT_SQUARE);
                 }
             }
             '}' => {
                 if stack.pop() != Some('}') {
-                    return SCORE_CURLY;
+                    return LineResult::Corrupt(CORRUPT_CURLY);
                 }
             }
             '>' => {
                 if stack.pop() != Some('>') {
-                    return SCORE_ANGLE;
+                    return LineResult::Corrupt(CORRUPT_ANGLE);
                 }
             }
             _ => panic!("Invalid character in input: {}", c),
         }
     }
-    0
+    if !stack.is_empty() {
+        // line is incomplete, whatever remains in stack are the missing characters
+        let score = stack.iter().rev().fold(0, |acc, c| {
+            (acc * 5)
+                + match c {
+                    ')' => INCOMPLETE_PAREN,
+                    ']' => INCOMPLETE_SQUARE,
+                    '}' => INCOMPLETE_CURLY,
+                    '>' => INCOMPLETE_ANGLE,
+                    // could have an Enum for characters in stack to make the match
+                    // exhaustive and avoid this panic!, but I found it overkill
+                    _ => panic!("Invalid character in stack, how could this happen?!: {}", c),
+                }
+        });
+        return LineResult::Incomplete(score);
+    }
+    LineResult::Valid
 }
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
+    let scores: Vec<LineResult> = input.lines().map(|l| score_line(l)).collect();
     println!(
         "Total syntax error score: {}",
-        input.lines().map(|l| score_line(l)).sum::<u32>(),
+        scores
+            .iter()
+            .map(|s| match s {
+                LineResult::Corrupt(score) => *score,
+                _ => 0,
+            })
+            .sum::<u32>(),
+    );
+    let mut incomplete_scores: Vec<u64> = scores
+        .iter()
+        .filter(|s| matches!(s, LineResult::Incomplete(_)))
+        .map(|s| match s {
+            LineResult::Incomplete(score) => *score,
+            _ => 0,
+        })
+        .collect();
+    incomplete_scores.sort_unstable();
+    println!(
+        "Middle completion score: {}",
+        incomplete_scores[incomplete_scores.len() / 2]
     );
 }
 
@@ -52,17 +99,44 @@ mod tests {
     #[test]
     fn test_score_line() {
         // valid lines
-        assert_eq!(score_line(""), 0);
-        assert_eq!(score_line("[]"), 0);
-        assert_eq!(score_line("{()()()}"), 0);
-        assert_eq!(score_line("[<>({}){}[([])<>]]"), 0);
+        assert_eq!(score_line(""), LineResult::Valid);
+        assert_eq!(score_line("[]"), LineResult::Valid);
+        assert_eq!(score_line("{()()()}"), LineResult::Valid);
+        assert_eq!(score_line("[<>({}){}[([])<>]]"), LineResult::Valid);
         // incomplete lines
-        assert_eq!(score_line("[({(<(())[]>[[{[]{<()<>>"), 0);
-        assert_eq!(score_line(""), 0);
+        assert_eq!(
+            score_line("[({(<(())[]>[[{[]{<()<>>"),
+            LineResult::Incomplete(288957)
+        );
+        assert_eq!(
+            score_line("[(()[<>])]({[<{<<[]>>("),
+            LineResult::Incomplete(5566)
+        );
+        assert_eq!(
+            score_line("(((({<>}<{<{<>}{[]{[]{}"),
+            LineResult::Incomplete(1480781)
+        );
+        assert_eq!(
+            score_line("{<[[]]>}<{[{[{[]{()[[[]"),
+            LineResult::Incomplete(995444)
+        );
+        assert_eq!(
+            score_line("<{([{{}}[<[[[<>{}]]]>[]]"),
+            LineResult::Incomplete(294)
+        );
         // corrupt lines
-        assert_eq!(score_line("[[<[([]))<([[{}[[()]]]"), 3);
-        assert_eq!(score_line("[{[{({}]{}}([{[{{{}}([]"), 57);
-        assert_eq!(score_line("{([(<{}[<>[]}>{[]{[(<()>"), 1197);
-        assert_eq!(score_line("<{([([[(<>()){}]>(<<{{"), 25137);
+        assert_eq!(score_line("[[<[([]))<([[{}[[()]]]"), LineResult::Corrupt(3));
+        assert_eq!(
+            score_line("[{[{({}]{}}([{[{{{}}([]"),
+            LineResult::Corrupt(57)
+        );
+        assert_eq!(
+            score_line("{([(<{}[<>[]}>{[]{[(<()>"),
+            LineResult::Corrupt(1197)
+        );
+        assert_eq!(
+            score_line("<{([([[(<>()){}]>(<<{{"),
+            LineResult::Corrupt(25137)
+        );
     }
 }
