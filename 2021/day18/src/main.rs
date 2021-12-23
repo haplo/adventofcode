b003fea300190e5d949b4ebf4022e204cb38df22
Fidel Ramos <f@fidelramos.net>
2021-12-23 22:41:03 +0000

    2021 Day 18, Part 2

--- 2021/day18/src/main.rs
+++ 2021/day18/src/main.rs
@@ -190,11 +190,21 @@ impl FromStr for SnailNum {
     }
 }
 
+impl Add for &SnailNum {
+    type Output = SnailNum;
+
+    fn add(self, rhs: Self) -> SnailNum {
+        let mut n = SnailNum::new(self.clone(), rhs.clone());
+        n.reduce();
+        n
+    }
+}
+
 impl Add for SnailNum {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
-        let mut n = SnailNum::new(self, other);
+    fn add(self, rhs: Self) -> Self {
+        let mut n = SnailNum::new(self, rhs);
         n.reduce();
         n
     }
@@ -219,16 +229,39 @@ impl Magnitude for SnailNum {
     }
 }
 
+fn find_largest_sum(nums: Vec<SnailNum>) -> u64 {
+    let mut best: u64 = u64::MIN;
+    for i in 0..nums.len() {
+        for j in 0..nums.len() {
+            if i == j {
+                continue;
+            }
+            let m = (&nums[i] + &nums[j]).magnitude();
+            if m > best {
+                best = m;
+            }
+        }
+    }
+    best
+}
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
+    let nums: Vec<SnailNum> = input
+        .lines()
+        .map(|l| l.parse::<SnailNum>().unwrap())
+        .collect();
     println!(
         "Magnitude of final result: {}",
-        input
-            .lines()
-            .map(|l| l.parse::<SnailNum>().unwrap())
+        nums.clone()
+            .into_iter()
             .reduce(|a, b| a + b)
             .unwrap()
             .magnitude()
+    );
+    println!(
+        "Largest magnitude of adding a pair: {}",
+        find_largest_sum(nums)
     );
 }
 
@@ -688,5 +721,25 @@ mod tests {
                 .magnitude(),
             3488
         );
+    }
+
+    #[test]
+    fn test_find_largest_sum() {
+        let nums: Vec<SnailNum> = vec![
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+            "[[[[5,4],[7,7]],8],[[8,3],8]]",
+            "[[9,3],[[9,9],[6,[4,9]]]]",
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]",
+        ]
+        .into_iter()
+        .map(|s| s.parse::<SnailNum>().unwrap())
+        .collect();
+        assert_eq!(find_largest_sum(nums), 3993);
     }
 }
