3ad04da608bb14a4c728798dfb0dd843a28549be
Fidel Ramos <f@fidelramos.net>
2022-12-03 10:19:08 +0000

    2022 Day 1, Part 1

---
+++ 2022/day1/part1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from itertools import takewhile
+
+def sum_calories(lines):
+    totals = []
+    it = iter(lines)
+    while True:
+        total = sum(int(n) for n in takewhile(bool, it))
+        if not total:
+            break
+        totals.append(total)
+    return totals
+
+def main():
+    with open("input.txt", "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+    totals = sum_calories(lines)
+    totals.sort(reverse=True)
+    print(f"Elf with the most calories: {totals[0]}")
+
+if __name__ == "__main__":
+    main()
